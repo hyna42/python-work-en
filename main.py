f2129e167cc918bcdb9d673778ad9f68b6b27ef4
hyna42 <bylo@duck.com>
2025-11-26 23:18:55 +0100

    ep4: variables

--- main.py
+++ main.py
@@ -2,20 +2,34 @@ print("Hello World!")
 # print("Hello World!")
 c = "hello"
 
+## Remarque : python est un langage a typage dynamique , l'interpréteur détecte directement le type de la données
 
-###### DATA TYPE ######
-print(type(c))
-#1. id(<object>)
-print("identifiant de c =>",id(c))
+###### VARIALES ######
+"""
+    Python a un typage dynamique : c'est l'interprétateur qui va déduire le type de la données
+
+    0b -> binaire
+    0x -> héxadécimal
+    0o -> octal
+
+    
+"""
+
+n = 0o42
+nb = 24e12 #42.10^12
+print(n) 
+nb='cc'
+print('type nb = ',type(nb))
+
+fname = "Bayee"
+fname = list(fname)
 
 
-#2. type(<object>)
-print(type(4))
-print(type(4.0))
-print(type('4'))
-print(type((4,))) #immuable, virgule obligatoirsi un seul élémenet de tuple
-print(type([4])) #mutable
-print(type({4}))
+lname = "Niass"
+lname = tuple(lname)
 
-#3. isInstance(<object>, <classInfo>)
-print("test valeur de vérité : 4 === string ? ", isinstance(4,int))
+print(fname)
+print(lname)
+
+print(type(fname), "AND",type(lname))
+
