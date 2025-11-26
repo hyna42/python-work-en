4d54954f96aefb71b222094149c86228f46ab1ee
hyna42 <bylo@duck.com>
2025-11-27 00:37:57 +0100

    ep9: conditions - match

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-###### LECTURE AU CLAVIER ######
+###### CONDITIONS ######
 
-#input() -> ecrit sur la sortie standard
-# converit en string
-#supprime le caratère de retrour à la ligne
-#Attention peut délecher erreur du genre EOF (sécu)
+#1. if-elif-else
+access = True
+access = 1
 
-name= input("votre prénom : ")
-print("value = ",name)
 
+
+print("1" == 1)
+
+if access:
+    print("Vous pouvez passer")
+else:
+    print("Ohhhhh")
+
+
+#2. match-case - depuis python 3.10
+"""
+    peut travailler sur des motifs
+"""
+role = "adminx"
+match role:
+    case "admin" | "user":
+        print("can access site")
+    case "visitor":
+        print("can register")
+    case _:
+        print("Unknown status")
