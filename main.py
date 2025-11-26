1ebf6a5c4d47520698b745dab4756d601f1d1150
hyna42 <bylo@duck.com>
2025-11-26 22:25:26 +0100

    feat(data-type): Id(), type(), isinstance()

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 print("Hello World!")
 # print("Hello World!")
+c = "hello"
+
+
+###### DATA TYPE ######
+print(type(c))
+#1. id(<object>)
+print("identifiant de c =>",id(c))
+
+
+#2. type(<object>)
+print(type(4))
+print(type(4.0))
+print(type('4'))
+print(type((4,))) #immuable, virgule obligatoirsi un seul élémenet de tuple
+print(type([4])) #mutable
+print(type({4}))
+
+#3. isInstance(<object>, <classInfo>)
+print("test valeur de vérité : 4 === string ? ", isinstance(4,int))
