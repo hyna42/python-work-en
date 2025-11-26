bf7b9a56f7d9ad972cbba94800f5211577e740be
hyna42 <bylo@duck.com>
2025-11-26 23:43:44 +0100

    ep7: lecture au clavier

--- main.py
+++ main.py
@@ -1,35 +1,10 @@
-print("Hello World!")
-# print("Hello World!")
-c = "hello"
+###### LECTURE AU CLAVIER ######
 
-## Remarque : python est un langage a typage dynamique , l'interpréteur détecte directement le type de la données
+#input() -> ecrit sur la sortie standard
+# converit en string
+#supprime le caratère de retrour à la ligne
+#Attention peut délecher erreur du genre EOF (sécu)
 
-###### VARIALES ######
-"""
-    Python a un typage dynamique : c'est l'interprétateur qui va déduire le type de la données
-
-    0b -> binaire
-    0x -> héxadécimal
-    0o -> octal
-
-    
-"""
-
-n = 0o42
-nb = 24e12 #42.10^12
-print(n) 
-nb='cc'
-print('type nb = ',type(nb))
-
-fname = "Bayee"
-fname = list(fname)
-
-
-lname = "Niass"
-lname = tuple(lname)
-
-print(fname)
-print(lname)
-
-print(type(fname), "AND",type(lname))
+name= input("votre prénom : ")
+print("value = ",name)
 
