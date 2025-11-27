f9105ece5253700a50fc7f32074733260bfa083c
hyna42 <bylo@duck.com>
2025-11-27 18:41:03 +0100

    feat(module) : import module

--- main.py
+++ main.py
@@ -1,37 +1,16 @@
-###### EP11 - FONCTION ######
+###### EP12 - MODULES ######
 
-# (1) définition fonctiuon
-def print_menu():
-    print("------MENU PRINCIPAL------")
-    print("1.Effectuer une commande")
-    print("2.Annuler une commande")
-    print("3. Afficher l'aide")
-    print("4.Quitter")
+# module - paquet
 
-print_menu()
+"""
+ import module_name ==> importe tout le module
+    use : module_name.paquet
 
-# (2) fonction avec paramètre obligatoire
+ from module_name import paquet
+"""
+  
+# import something 
+from something import say_hello , get_max_number
 
-def says(author, message):
-    print(author, ":", message)
-
-says("Amadou","hello world")
-says("Baye","dieuredieufé Niass")
-
-# (3) fonction avec paramètre optionnels
-def create_player(name,level=1):
-    print(name, "de niveau",level)
-
-create_player("Chuck",7)
-create_player("Daddy")
-
-# (4) fonction renvoyant un résultat
-def sum_integer(a,b):
-    return a + b
-result = sum_integer(4,8)
-print(type(result))
-print(result)
-
-# (5) fonction anonyme
-double = lambda n : n*2
-print(double(2))
+say_hello()
+print(get_max_number(4,8))
