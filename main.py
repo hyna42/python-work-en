f6dca7becadfb69231d3f3856a2d6c0237ca696d
hyna42 <bylo@duck.com>
2025-11-27 18:55:22 +0100

    feat(function): define eecution script, by the file to execute

--- main.py
+++ main.py
@@ -11,6 +11,11 @@
   
 # import something 
 from something import say_hello , get_max_number
+from something import return_sum_of_two_numbers as sum
 
 say_hello()
 print(get_max_number(4,8))
+print("sum:",sum(4,6))
+
+if __name__ == '__main__':
+    print("Je suis dans main")
--- something.py
+++ something.py
@@ -4,3 +4,10 @@ def say_hello():
 
 def get_max_number(a,b):
     return (a if a>b else b);
+
+
+def return_sum_of_two_numbers(a,b):
+    return a+b
+
+if __name__ == '__main__':
+    print("Je suis dans something")
