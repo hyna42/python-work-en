020484797008254ca236d694213a8a3a11573b7b
hyna42 <bylo@duck.com>
2025-11-27 19:29:05 +0100

    feat(module) : test paquets

--- main.py
+++ main.py
@@ -9,10 +9,14 @@
  from module_name import paquet
 """
   
-# import something 
+# import modules 
 from something import say_hello , get_max_number
 from something import return_sum_of_two_numbers as sum
 
+#import paquet
+from calcul.operations import addition,soustraction,multiplication,division
+
+#################### MAIN #####################
 say_hello()
 print(get_max_number(4,8))
 print("sum:",sum(4,6))
@@ -22,3 +26,9 @@ if __name__ == '__main__':
 
 
 # (2) paquet : c'est un dossier contenant 1 fichier __init__.py et un et modules (.py files): exemple => paquet /calcul
+print(addition(4,9))
+print(soustraction(8,6))
+print(soustraction(7,6))
+print(division(5,0))
+
+
