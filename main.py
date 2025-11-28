4133bf1d5271a7c99f986afc1f7b88558e0ffed8
hyna42 <bylo@duck.com>
2025-11-28 20:55:24 +0100

    feat(str): role des f-string

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 ###### EP15 - Chaines de caractères ######
-import datetime
+from datetime import datetime
 #str : séquentiel, immuable, indexable,itérable
 
 ## I. INTRO
@@ -14,10 +14,30 @@ print(s[::-1]) #invrser chaine de caractère
 
 # (4) méthodes de séquence <sequence>.index()  <sequence>.count()
 
-## II. FORMATAGE
+## II. FORMATAGE :f-string f""
 print(f"Ma Beugue {s}")
 
-print("*" *50)
 
-now = datetime.datetime.now()
+now = datetime.now()
 print(f"{now:%d-%m-%Y}")
+print("*" *100)
+
+print(f"Je m'appelle {s.upper()}")
+
+pi = 314159
+number = 42
+print(f"Le nombre pi arrondi  à 2 décimales est : {number:>10}")
+
+"""
+    Les f-string permettent d'ajouter des variables et des expressions dans des chaines de caractères, ils permettent également des des formattage avancées tels que : 
+    
+    * arrondir des flottants à un certain décimal avec le spécificatern :.2f
+    * ajouter des sépaateur de miliers avec :,
+    * remplissage de nombre avec des zero exempel :04 (4 nombre)
+    * fromatage de pourcentage : :.1% (1 chiffre après la virgule)
+    * alignement et largeur minimale : < (gauche), > (droite), ^ (centre)
+    * gestion des dates et des heures
+
+
+
+"""
